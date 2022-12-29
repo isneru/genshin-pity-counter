af29ca57083abb1ade15102d3cdfc81b7c1d1e9c
isneru <diogoclanogueira@gmail.com>
2022-12-29 18:22:48 +0000

    test SEO

--- src/components/NextHead/NextHead.tsx
+++ src/components/NextHead/NextHead.tsx
@@ -10,6 +10,9 @@ export const NextHead = ({ title, icon }: NextHeadProps) => {
     <Head>
       <title>{title}</title>
       <link rel="icon" href={icon} type="image/svg+xml" />
+      <meta property="og:title" content={title} />
+      <meta property="og:type" content="blog" />
+      <meta property="og:image" content="/logo.svg" />
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
